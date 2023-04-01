332db7735fafa4152993b86ced97f60b3d08cf5c
Harold Jiang <jzsues@gmail.com>
2023-04-01 22:28:35 +0800

    Update GPTQ_loader.py

--- modules/GPTQ_loader.py
+++ modules/GPTQ_loader.py
@@ -15,7 +15,7 @@ from modelutils import find_layers
 from quant import make_quant
 
 
-def _load_quant(model, checkpoint, wbits, groupsize=-1, faster_kernel=False, exclude_layers=['lm_head'], kernel_switch_threshold=128):
+def _load_quant(model, checkpoint, wbits, groupsize=-1, exclude_layers=['lm_head'], kernel_switch_threshold=128):
     config = AutoConfig.from_pretrained(model)
     def noop(*args, **kwargs):
         pass
